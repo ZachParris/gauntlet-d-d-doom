d4717e58b8a131a06b71bc24bf2b684649e63d66
Paul <wip321@yahoo.com>
2016-04-22 17:54:24 -0500

    IIFEs made and everything converted into IIFEs fun times...

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,124 +1,130 @@
-var lastId;
-// This is the Player variable
-var Player;
-/*
-  Test code to generate a human player and an orc player
- */
-var warrior = new Gauntlet.Combatants.Human();
-warrior.setWeapon(new WarAxe());
-warrior.generateClass();  // This will be used for "Surprise me" option
-console.log(warrior.toString());
-
-var orc = new Gauntlet.Combatants.Orc();
-orc.generateClass();
-orc.setWeapon(new BroadSword());
-console.log(orc.toString());
-
-/*
-  Test code to generate a spell
- */
-var spell = new Gauntlet.SpellBook.Sphere();
-console.log("spell: ", spell.toString());
-
-
-$(document).ready(function() {
+var Game = (function(oldGame) {
+  var lastId;
+  // This is the Player variable
+  var Player;
+  oldGame.getPlayer = function() {
+    return Player
+  }
   /*
-    Show the initial view that accepts player name
+    Test code to generate a human player and an orc player
    */
-  $("#player-setup").show();
+  var warrior = new Gauntlet.Combatants.Human();
+  warrior.setWeapon(new WarAxe());
+  warrior.generateClass();  // This will be used for "Surprise me" option
+  console.log(warrior.toString());
+
+  var orc = new Gauntlet.Combatants.Orc();
+  orc.generateClass();
+  orc.setWeapon(new BroadSword());
+  console.log(orc.toString());
+
   /*
-    When any button with card__link class is clicked,
-    move on to the next view.
+    Test code to generate a spell
    */
+  var spell = new Gauntlet.SpellBook.Sphere();
+  console.log("spell: ", spell.toString());
 
-   //Click to choose the weapon and class
-  $("body").click(function(e){
-    if (e.target.id !== "") {
-      lastId = e.target.id;
-    }
-  })
-  $(".card__link").click(function(e) {
-    var nextCard = $(this).attr("next");
-    var moveAlong = false;
 
-    switch (nextCard) {
-      case "card--class":
-        moveAlong = ($("#player-name").val() !== "");
-        Player = new Gauntlet.Combatants.Player($("#player-name").val());
-        break;
-      case "card--weapon":
-        moveAlong = (lastId !== "");
-        switch (lastId) {
-          case "Warrior":
-            Player.class = new Gauntlet.GuildHall.Warrior();
-            break;
-          case "Wizard":
-            Player.class = new Gauntlet.GuildHall.Wizard();
-            break;
-          case "Thief":
-            Player.class = new Gauntlet.GuildHall.Thief();
-            break;
-          case "Valkyrie":
-            Player.class = new Gauntlet.GuildHall.Valkyrie();
-            break;
-          case "Sorcerer":
-            Player.class = new Gauntlet.GuildHall.Sorcerer();
-            break;
-          case "Ninja":
-            Player.class = new Gauntlet.GuildHall.Ninja();
-            break;
-          case "Berserker":
-            Player.class = new Gauntlet.GuildHall.Berserker();
-            break;
-          case "Conjurer":
-            Player.class = new Gauntlet.GuildHall.Conjurer();
-            break;
-          case "Assassin":
-            Player.class = new Gauntlet.GuildHall.Assassin();
-            break;
-          case "Monk":
-            Player.class = new Gauntlet.GuildHall.Monk();
-            break;
-          case "Suprise":
-            Player.class = new Gauntlet.GuildHall.PlayerClass();
-            break;
-        }
-        lastId = "";
-        break;
-      case "card--battleground":
-        moveAlong = (lastId !== "");
-        switch (lastId) {
-          case "BroadSword":
-          Player.weapon = new BroadSword();
-          break;
-          case "WarAxe":
-          Player.weapon = new WarAxe();
-          break;
-          case "Dagger":
-          Player.weapon = new Dagger();
-          break;
-        }
-        lastId = "";
-        startBattle();
-        newEnemy();
-        update();
-        break;
-    }
+  $(document).ready(function() {
+    /*
+      Show the initial view that accepts player name
+     */
+    $("#player-setup").show();
+    /*
+      When any button with card__link class is clicked,
+      move on to the next view.
+     */
 
-    if (moveAlong) {
+     //Click to choose the weapon and class
+    $("body").click(function(e){
+      if (e.target.id !== "") {
+        lastId = e.target.id;
+      }
+    })
+    $(".card__link").click(function(e) {
+      var nextCard = $(this).attr("next");
+      var moveAlong = false;
+
+      switch (nextCard) {
+        case "card--class":
+          moveAlong = ($("#player-name").val() !== "");
+          Player = new Gauntlet.Combatants.Player($("#player-name").val());
+          break;
+        case "card--weapon":
+          moveAlong = (lastId !== "");
+          switch (lastId) {
+            case "Warrior":
+              Player.class = new Gauntlet.GuildHall.Warrior();
+              break;
+            case "Wizard":
+              Player.class = new Gauntlet.GuildHall.Wizard();
+              break;
+            case "Thief":
+              Player.class = new Gauntlet.GuildHall.Thief();
+              break;
+            case "Valkyrie":
+              Player.class = new Gauntlet.GuildHall.Valkyrie();
+              break;
+            case "Sorcerer":
+              Player.class = new Gauntlet.GuildHall.Sorcerer();
+              break;
+            case "Ninja":
+              Player.class = new Gauntlet.GuildHall.Ninja();
+              break;
+            case "Berserker":
+              Player.class = new Gauntlet.GuildHall.Berserker();
+              break;
+            case "Conjurer":
+              Player.class = new Gauntlet.GuildHall.Conjurer();
+              break;
+            case "Assassin":
+              Player.class = new Gauntlet.GuildHall.Assassin();
+              break;
+            case "Monk":
+              Player.class = new Gauntlet.GuildHall.Monk();
+              break;
+            case "Suprise":
+              Player.class = new Gauntlet.GuildHall.PlayerClass();
+              break;
+          }
+          lastId = "";
+          break;
+        case "card--battleground":
+          moveAlong = (lastId !== "");
+          switch (lastId) {
+            case "BroadSword":
+            Player.weapon = new BroadSword();
+            break;
+            case "WarAxe":
+            Player.weapon = new WarAxe();
+            break;
+            case "Dagger":
+            Player.weapon = new Dagger();
+            break;
+          }
+          lastId = "";
+          Game.startBattle();
+          Game.newEnemy();
+          Game.update();
+          break;
+      }
+
+      if (moveAlong) {
+        $(".card").hide();
+        $("." + nextCard).show();
+      }
+
+
+    /*
+      When the back button clicked, move back a view
+     */
+    $(".card__back").click(function(e) {
+      var previousCard = $(this).attr("previous");
       $(".card").hide();
-      $("." + nextCard).show();
-    }
+      $("." + previousCard).show();
+    });
 
-
-  /*
-    When the back button clicked, move back a view
-   */
-  $(".card__back").click(function(e) {
-    var previousCard = $(this).attr("previous");
-    $(".card").hide();
-    $("." + previousCard).show();
   });
-
-});
-});
+  });
+  return oldGame
+})(Game || {})
--- javascripts/battle.js
+++ javascripts/battle.js
@@ -1,38 +1,42 @@
-//gives health bonus Player needs, etc..wrap in IIFE name Game
-function startBattle() {
-Player.health += Player.class.healthBonus
-Player.strength += Player.class.strengthBonus + Player.weapon.damage
-Player.intelligence += Player.class.intelligenceBonus
+var Game = (function(oldGame) {
+  //gives health bonus Player needs, etc..wrap in IIFE name Game
+  var Player
+  oldGame.startBattle = function() {
+    Player = Game.getPlayer()
+    Player.health += Player.class.healthBonus
+    Player.strength += Player.class.strengthBonus + Player.weapon.damage
+    Player.intelligence += Player.class.intelligenceBonus
   };
 
-$("#attackButton").click(function() {
-  orc.health -= Player.strength
-  Player.health -= orc.strength
-  update()
-  if (Player.health === 0) {
-    playerLost()
-  }  else if (orc.health === 0) {
-    playerWin()
-  }
-
+  $("#attackButton").click(function() {
+    orc.health -= Player.strength
+    Player.health -= orc.strength
+    Game.update()
+    if (Player.health === 0) {
+      playerLost()
+    }  else if (orc.health === 0) {
+      playerWin()
+    }
   });
 
-function update() {
-$("#player-stat").html(Player.toString())
-$("#enemy-stat").html(orc.toString())
+  oldGame.update = function() {
+    $("#player-stat").html(Player.toString())
+    $("#enemy-stat").html(orc.toString())
   };
 
-  function newEnemy() {
+  oldGame.newEnemy = function() {
     orc = new Gauntlet.Combatants.Orc();
     orc.generateClass();
     orc.setWeapon(new BroadSword());
     orc.health += orc.class.healthBonus
     orc.strength = orc.class.strengthBonus + orc.weapon.damage
   };
-function playerLost() {}
-function playerWin() {}
-// #result
-// #win-loss
-// #playAgain
+  function playerLost() {}
+  function playerWin() {}
+  // #result
+  // #win-loss
+  // #playAgain
 
 
+  return oldGame
+})(Game || {})
